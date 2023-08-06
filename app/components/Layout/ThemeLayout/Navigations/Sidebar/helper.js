a38f24e79fe6cedff11cf65e25482d3117b1b210
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-08-06 22:27:24 +0200

    algo gestion de perm en local

---
+++ app/components/Layout/ThemeLayout/Navigations/Sidebar/helper.js
@@ -0,0 +1,44 @@
+export const access = [
+  {
+    page: "Accueil",
+    path: "/",
+    isCanAdd: false,
+    isCanUpdate: false,
+    isCanDelete: false,
+  },
+  {
+    page: "MDT",
+    path: "/mdt/encodage/civils",
+    isCanAdd: false,
+    isCanUpdate: false,
+    isCanDelete: false,
+  },
+  {
+    page: "MDT",
+    path: "/mdt/encodage/armes",
+    isCanAdd: true,
+    isCanUpdate: false,
+    isCanDelete: false,
+  },
+  {
+    page: "MDT",
+    path: "/mdt/panic-bouton",
+    isCanAdd: true,
+    isCanUpdate: false,
+    isCanDelete: false,
+  },
+  {
+    page: "MDT",
+    path: "/mdt/encodage/vehicules",
+    isCanAdd: true,
+    isCanUpdate: false,
+    isCanDelete: false,
+  },
+  {
+    page: "Services",
+    path: "/services/feuille-d-heures",
+    isCanAdd: true,
+    isCanUpdate: false,
+    isCanDelete: false,
+  },
+];
