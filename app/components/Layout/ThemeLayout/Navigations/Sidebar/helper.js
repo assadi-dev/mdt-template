6559ac27e2c4809ec3756b75de800d766538cecd
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-08-08 11:59:35 +0200

    Algo Check permission fonctionnel à 100%

--- app/components/Layout/ThemeLayout/Navigations/Sidebar/helper.js
+++ app/components/Layout/ThemeLayout/Navigations/Sidebar/helper.js
@@ -2,13 +2,13 @@ export const access = [
   {
     page: "Accueil",
     path: "/",
-    isCanAdd: false,
+    isCanAdd: true,
     isCanUpdate: false,
     isCanDelete: false,
   },
   {
     page: "MDT",
-    childrens: "Encodage/Civils",
+    childrens: "Encodage",
     path: "/mdt/encodage/civils",
     isCanAdd: false,
     isCanUpdate: false,
@@ -16,7 +16,7 @@ export const access = [
   },
   {
     page: "MDT",
-    childrens: "Encodages/Armes",
+    childrens: "Encodages",
     path: "/mdt/encodage/armes",
     isCanAdd: true,
     isCanUpdate: false,
@@ -32,7 +32,7 @@ export const access = [
   },
   {
     page: "MDT",
-    childrens: "Encodage/Véhicules/Voitures",
+    childrens: "Encodage",
     path: "/mdt/encodage/vehicules",
     isCanAdd: true,
     isCanUpdate: false,
@@ -47,3 +47,40 @@ export const access = [
     isCanDelete: false,
   },
 ];
+
+/**
+ * Mapping des routes avec les permission
+ * @param {*} route
+ * @returns
+ */
+export const checkRoutes = (route) => {
+  if (route.childrens) {
+    let test = route.childrens.map((rc) => checkRoutes(rc));
+    return { name: route.name, childrens: test };
+  } else {
+    if (route.path) {
+      let fa = access.find((ra) => ra.path == route.path);
+      if (!fa) return [];
+      const { isCanAdd, isCanDelete, isCanUpdate } = fa;
+      return { ...route, isCanAdd, isCanDelete, isCanUpdate };
+    }
+  }
+};
+
+/**
+ * Suppression des routes avec enfant vides
+ * @param {*} route
+ * @returns
+ */
+export const filtrerEmptyObjets = (route) => {
+  return route.filter((objet) => {
+    if (Object.keys(objet).length == 0) {
+      return false;
+    }
+    if (objet.childrens && Array.isArray(objet.childrens)) {
+      objet.childrens = filtrerEmptyObjets(objet.childrens);
+      return objet.childrens.length > 0;
+    }
+    return true;
+  });
+};
--- app/components/Layout/ThemeLayout/Navigations/Sidebar/index.jsx
+++ app/components/Layout/ThemeLayout/Navigations/Sidebar/index.jsx
@@ -5,88 +5,24 @@ import SidebarItems from "./SidebarItems";
 import { sidebarRoutes } from "../../../../../routes/sidebarRoutes";
 import { useSelector } from "react-redux";
 import { factionsCollections } from "../../../../../config/factions";
-import { access } from "./helper";
+import { checkRoutes, filtrerEmptyObjets } from "./helper";
 
 const Sidebar = () => {
   const sideBarHeaderRef = useRef(null);
   const authenticateUser = useSelector((state) => state.AuthenticateReducer);
   const faction = authenticateUser.faction;
-
   const [accesRoutes, setAccessRoutes] = useState([]);
-  let authorize = [];
 
-  const accessChildrenToArray = () => {
-    let mapping = [...access].map((ac) => {
-      if (ac.childrens) {
-        if (ac.childrens.includes("/")) {
-          let list = ac.childrens.split("/");
-          list.map((l) => ({ childrens: l }));
-          return { ...ac, childrens: list };
-        }
-        return { ...ac, childrens: [ac.childrens] };
-      } else {
-        return ac;
-      }
-    });
-
-    return mapping;
-  };
-  const accessMapRoutes = accessChildrenToArray();
-
-  //console.log(accessMapRoutes);
-  const getSubPath = (childrens) => {
-    if (childrens) {
-      // childrens.map(chil=>getSubPath(chil))
-      // return getSubPath(childrens.childrens);
-    } else {
-      let fc = access.find((c) => c.path == childrens.path);
-      //console.log(fc);
-      // return fc;
-    }
-  };
-
-  const getParentRoutes = (parent) => {
-    console.log(test.find((t) => t[1].name == "MDT"));
-    console.log(parent);
-  };
-
-  const checkRoutes = (route) => {
-    if (route.childrens) {
-      let test = route.childrens.map((rc) => checkRoutes(rc));
-      return { name: route.name, childrens: test };
-    } else {
-      if (route.path) {
-        let fa = access.find((ra) => ra.path == route.path);
-        if (!fa) return [];
-        const { isCanAdd, isCanDelete, isCanUpdate } = fa;
-        return { ...route, isCanAdd, isCanDelete, isCanUpdate };
-      }
-    }
-  };
-
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!faction) return;
     const factionEmblem = faction ? factionsCollections[faction].emblem : "";
     if (sideBarHeaderRef.current)
       sideBarHeaderRef.current.style.backgroundImage = `url(${factionEmblem})`;
 
-    const cleanArray = (mf) => {
-      if (mf.childrens) {
-        return cleanArray(mf.childrens);
-      } else {
-        return mf.length > 0 || mf.path;
-      }
-    };
-    // console.log(access);
-    let mapRoute = [...sidebarRoutes]
-      .map((sbr) => checkRoutes(sbr))
-      .filter((clean) => cleanArray(clean));
-
-    console.log(mapRoute);
+    let mapRoute = [...sidebarRoutes].map((sbr) => checkRoutes(sbr));
+    mapRoute = filtrerEmptyObjets(mapRoute);
 
     setAccessRoutes((current) => (current = mapRoute));
-
-    // console.log(accesRoutes);
   }, [sideBarHeaderRef, faction]);
 
   return (
