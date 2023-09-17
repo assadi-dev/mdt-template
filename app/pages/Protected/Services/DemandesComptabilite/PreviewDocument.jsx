b93bf44e906e6fe33b4df5be4dc5cb3df1610650
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-09-17 13:42:58 +0200

    Update PreviewDocument.jsx

--- app/pages/Protected/Services/DemandesComptabilite/PreviewDocument.jsx
+++ app/pages/Protected/Services/DemandesComptabilite/PreviewDocument.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  PreviewDocumentBody,
   PreviewDocumentContainer,
   PreviewDocumentHeader,
   TextContent,
@@ -18,30 +19,35 @@ const PreviewDocument = ({ previewData, onCloseModal, ...props }) => {
   return (
     <PreviewDocumentContainer {...props}>
       <HeaderModal className="mb-3">
-        <p className="title">Demande de comptabilité </p>
+        <p className="form-title">Demande de comptabilité </p>
         <CloseModalBtn className="close-section" onClick={onCloseModal} />
       </HeaderModal>
-      <Row style={{ marginTop: "18px" }}>
-        <p className="agent">{agent}</p>{" "}
-      </Row>
-      <Row>
-        <p className="objet">
-          <strong>Objet: </strong> {objet}{" "}
-        </p>
-      </Row>
-      <Row className="justiy-content-end ">
-        <p className="date">{date}</p>
-      </Row>
-      <Row className="justiy-content-end">
-        <p className="montant">
-          <strong>Montant: </strong>
-          {montant}$
-        </p>
-      </Row>
+      <PreviewDocumentBody>
+        <Row style={{ marginTop: "18px" }}>
+          <p className="agent">
+            {" "}
+            <strong>Agent: </strong> {agent}
+          </p>{" "}
+        </Row>
+        <Row>
+          <p className="objet">
+            <strong>Objet: </strong> {objet}{" "}
+          </p>
+        </Row>
+        <Row className="justiy-content-end ">
+          <p className="date">{date}</p>
+        </Row>
+        <Row className="justiy-content-end">
+          <p className="montant">
+            <strong>Montant: </strong>
+            {montant} $
+          </p>
+        </Row>
 
-      <TextContent>
-        <MarkdownPreview className="theme-markdownPreview" source={raison} />
-      </TextContent>
+        <TextContent>
+          <MarkdownPreview className="theme-markdownPreview" source={raison} />
+        </TextContent>
+      </PreviewDocumentBody>
     </PreviewDocumentContainer>
   );
 };
