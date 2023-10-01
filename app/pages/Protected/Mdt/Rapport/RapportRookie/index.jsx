6d3aebb79d8d0d8f247f1f70e26d9705f00fb93b
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-10-01 23:51:02 +0200

    Tableau rapport Rookie

--- app/pages/Protected/Mdt/Rapport/RapportRookie/index.jsx
+++ app/pages/Protected/Mdt/Rapport/RapportRookie/index.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import {
+  AddBtnRapportRookie,
+  RapportRookiePageContainer,
+} from "./RapportRookie.styled";
+import { RowAction } from "../../../../../components/PageContainer";
+import DataTable from "../../../../../components/DataTable";
+import ShowRapportRookie from "./ShowRapportRookie";
 
 const RapportRookie = () => {
-  return <div>Rapport Rookie</div>;
+  const handleClickAddbtn = () => {};
+
+  const COLUMNS = [
+    {
+      Header: "Matricule",
+      accessor: "matricule",
+    },
+    {
+      Header: "Agent",
+      accessor: "agent",
+    },
+    {
+      Header: "Matricule du rookie",
+      accessor: "matriculeRookie",
+    },
+    {
+      Header: "Nom du rookie",
+      accessor: "rookieName",
+    },
+    {
+      Header: "Date et heure",
+      accessor: "createdAt",
+    },
+    {
+      Header: "Action",
+      accessor: "",
+      Cell: () => <ShowRapportRookie />,
+    },
+  ];
+
+  return (
+    <RapportRookiePageContainer>
+      <RowAction style={{ height: "100px" }}>
+        <div></div>
+        <AddBtnRapportRookie
+          className="bg-btn-alt-theme-color"
+          onClick={handleClickAddbtn}
+        >
+          Ajouter
+        </AddBtnRapportRookie>
+      </RowAction>
+      <DataTable
+        className="table"
+        columns={COLUMNS}
+        isLoading={false}
+        isSuccess={true}
+      />
+    </RapportRookiePageContainer>
+  );
 };
 
 export default RapportRookie;
