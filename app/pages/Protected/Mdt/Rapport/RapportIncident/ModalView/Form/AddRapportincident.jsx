ebeb4e879df8d2b1d161542abee126821d73db1c
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-10-22 18:47:16 +0200

    update

--- app/pages/Protected/Mdt/Rapport/RapportIncident/ModalView/Form/AddRapportincident.jsx
+++ app/pages/Protected/Mdt/Rapport/RapportIncident/ModalView/Form/AddRapportincident.jsx
@@ -5,7 +5,11 @@ import CloseModalBtn from "../../../../../../../components/Modal/CloseModalBtn";
 import FormRapportIncident from "./FormRapportIncident";
 import { defaultFormValues } from "./helpers";
 
+<<<<<<< Updated upstream
 const AddRapportIncident = ({ onCloseModal, ...props }) => {
+=======
+const AddRapportincident = ({ onCloseModal, ...props }) => {
+>>>>>>> Stashed changes
   return (
     <ModalRapportIncident {...props}>
       <HeaderModal>
@@ -17,4 +21,8 @@ const AddRapportIncident = ({ onCloseModal, ...props }) => {
   );
 };
 
+<<<<<<< Updated upstream
 export default AddRapportIncident;
+=======
+export default AddRapportincident;
+>>>>>>> Stashed changes
--- app/pages/Protected/Mdt/Rapport/RapportIncident/ModalView/listOfRapportIncidentView.jsx
+++ app/pages/Protected/Mdt/Rapport/RapportIncident/ModalView/listOfRapportIncidentView.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+<<<<<<< Updated upstream
 
+=======
+import AddRapportincident from "./Form/AddRapportincident";
+>>>>>>> Stashed changes
 import EditRapportIncident from "./Form/EditRapportIncident";
 import PreviewRapportIncident from "./Form/PreviewRapportIncident";
 import DeleteRapportIncident from "./Form/DeleteRapportIncident";
