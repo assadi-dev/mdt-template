6a0996909ffcecd3e97b61844f1d571cd4b1a1d2
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-02 22:46:13 +0100

    Update helpers.js

--- app/pages/Protected/Mdt/Rapport/RapportIncident/ModalView/Form/helpers.js
+++ app/pages/Protected/Mdt/Rapport/RapportIncident/ModalView/Form/helpers.js
@@ -1,14 +1,30 @@
+import * as yup from "yup";
+import { requiredMessage } from "../../../../../../../config/ValidationMessage";
+import Api from "../../../../../../../services/api/instance";
+
 export const defaultFormValues = {
-  id: "",
-  matricule: "",
-  idAgent: "",
   agent: "",
-  officierimplique: "",
-  typeIncident: "",
-  createdAt: "",
-  emplacement: "",
-  numeraRapport: "",
-  corpsIncident: "",
+  officerImplicated: "",
+  incidentType: "",
+  location: "",
+  commentText: "",
 };
 
 export const inputOption = { required: true };
+
+export const incidentReportResolver = yup.object().shape({
+  officerImplicated: yup.string().required(requiredMessage),
+  incidentType: yup.string().required(requiredMessage),
+  location: yup.string().required(requiredMessage),
+  commentText: yup.string().required(requiredMessage),
+});
+
+export const saveCreateRapportIncident = (body) => {
+  return Api.post("/incident_reports", body);
+};
+export const saveUpdateRapportIncident = (id, body) => {
+  return Api.put(`/incident_reports/${id}`, body);
+};
+export const saveDeleteRapportIncident = (id) => {
+  return Api.delete(`/incident_reports/${id}`);
+};
