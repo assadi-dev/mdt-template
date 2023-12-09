306b8add58f12f4e3532cc23a724267aec8dbefc
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-09 14:59:40 +0100

    Update helpers.js

--- app/pages/Protected/Mdt/Rapport/RapportRookie/ModalView/Form/helpers.js
+++ app/pages/Protected/Mdt/Rapport/RapportRookie/ModalView/Form/helpers.js
@@ -3,3 +3,7 @@ import Api from "../../../../../../../services/api/instance";
 export const saveCreateRapportRookie = (body) => {
   return Api.post(`/rookie_report/create`, body);
 };
+
+export const fetchRookieLists = (params) => {
+  return Api.get(`/agents/rookies/list}`, params);
+};
