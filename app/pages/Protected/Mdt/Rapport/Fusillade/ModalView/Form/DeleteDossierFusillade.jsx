2c063371e69b3bdb4434fb508b2922c6fe391187
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-10 15:17:14 +0100

    Suppression rapport fusillade OK

--- app/pages/Protected/Mdt/Rapport/Fusillade/ModalView/Form/DeleteDossierFusillade.jsx
+++ app/pages/Protected/Mdt/Rapport/Fusillade/ModalView/Form/DeleteDossierFusillade.jsx
@@ -2,13 +2,38 @@ import React from "react";
 import DeleteConfirmForm from "../../../../../../../components/Modal/DialogConfirm/DeleteConfirmForm";
 import { useDispatch, useSelector } from "react-redux";
 import useProcess from "../../../../../../../hooks/useProcess";
+import { delete_GunFightReport } from "../../helpers";
+import { deleteGunFightReport } from "../../../../../../../features/GunFightReport/GunFightReport.slice";
+import {
+  toastError,
+  toastSuccess,
+} from "../../../../../../../services/utils/alert";
 
 const DeleteDossierFusillade = ({ payload, onCloseModal, ...props }) => {
   const LABEL_TEXT = `Voulez-vous supprimer le dossier fusillade n° ${
-    payload?.numeroDossier || "????"
+    payload?.numeroReport || "????"
   } ?`;
+  const DELETE_SUCCESS = `Le rapport n° ${
+    payload?.numeroReport || "????"
+  }  à bien été supprimé`;
   const dispatch = useDispatch();
   const { process, toggleProcess } = useProcess();
+  const ID = payload?.id;
+
+  const handleDeleteConfirm = async () => {
+    try {
+      toggleProcess();
+
+      await delete_GunFightReport(ID);
+      dispatch(deleteGunFightReport([ID]));
+      onCloseModal();
+      toastSuccess(DELETE_SUCCESS);
+    } catch (error) {
+      toastError();
+    } finally {
+      toggleProcess;
+    }
+  };
 
   return (
     <DeleteConfirmForm
@@ -16,6 +41,7 @@ const DeleteDossierFusillade = ({ payload, onCloseModal, ...props }) => {
       onCloseModal={onCloseModal}
       {...props}
       process={process}
+      onConfirm={handleDeleteConfirm}
     ></DeleteConfirmForm>
   );
 };
