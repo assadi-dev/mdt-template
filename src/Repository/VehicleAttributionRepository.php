27d5b408901909eda8167df738b4d01606dcc0cd
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-28 01:31:13 +0100

    Api récuperation des attribution vehicle

--- src/Repository/VehicleAttributionRepository.php
+++ src/Repository/VehicleAttributionRepository.php
@@ -2,9 +2,13 @@
 
 namespace App\Repository;
 
+use App\Entity\Agent;
+use App\Entity\Grade;
 use App\Entity\VehicleAttribution;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<VehicleAttribution>
@@ -39,28 +43,75 @@ class VehicleAttributionRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return VehicleAttribution[] Returns an array of VehicleAttribution objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('v.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return VehicleAttribution[] Returns an array of VehicleAttribution objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('v')
+    //            ->andWhere('v.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('v.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?VehicleAttribution
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?VehicleAttribution
+    //    {
+    //        return $this->createQueryBuilder('v')
+    //            ->andWhere('v.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+
+
+    public function findByPagination($items_per_page, $page, $search)
+    {
+        $countResult = ($page - 1) * $items_per_page;
+
+        $qb = $this->createQueryBuilder("va");
+        $qb->select("
+            va.id,
+            va.agentAttributed as idAgentAttributed,
+
+            CONCAT( agAttributed.matricule,'-',agAttributed.firstname,' ',agAttributed.lastname) as agentAttributed,
+
+            g.name as grade,
+            va.typeVehicle,
+            va.immatriculation,
+            va.idVehicle,
+            va.agent,
+            va.createdAt
+        ")
+        ->leftJoin(Agent::class, "ag", "WITH", "ag.id = va.agent")
+        ->leftJoin(Agent::class, "agAttributed", "WITH", "agAttributed.id = va.agentAttributed")
+        ->leftJoin(Grade::class, "g", "WITH", "g.id = agAttributed.grade")
+/*         ->orHaving($qb->expr()->like("va.immatriculation", ":search"))
+        ->orHaving($qb->expr()->like("va.typeVehicle", ":search"))
+        ->orHaving($qb->expr()->like("agentAttributed", ":search"))
+        ->orHaving($qb->expr()->like("g.name", ":search"))
+        ->setParameter("search", "%$search%") */
+        ->orderBy("va.createdAt", "DESC")
+        ->groupBy("va.id")
+        ;
+
+        $criteria = Criteria::create()
+        ->setFirstResult($countResult)
+        ->setMaxResults($items_per_page);
+        $qb->addCriteria($criteria);
+        $query =  $qb->getQuery();
+
+        $paginator = new Paginator($query, false);
+        $count =  $paginator->count();
+
+        $result = $qb->getQuery()->getResult();
+        return ["count" => $count,"data" => $result];
+
+    }
+
+
 }
