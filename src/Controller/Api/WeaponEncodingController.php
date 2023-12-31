c92abaec61ff7f67a781a32401b2c7e64107331a
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-31 12:34:26 +0100

    Api récuperation armes de l'agent

--- src/Controller/Api/WeaponEncodingController.php
+++ src/Controller/Api/WeaponEncodingController.php
@@ -51,6 +51,44 @@ class WeaponEncodingController extends AbstractController
         }
     }
     /**
+     * @Route("api/service_weapon_encodings/agent/{idAgent}", name="app_get_service_weapon_encoding_byAgent", methods="GET"  )
+     */
+    public function get_weapon_enconding_of_service_byAgent($idAgent, Request $request)
+    {
+        try {
+            $item_per_page = $request->query->get("item_per_page");
+            $search =  $request->query->get("search");
+            $page =  $request->query->get("page");
+            if(!isset($item_per_page)) {
+                $item_per_page = 5;
+            }
+            if(!isset($search)) {
+                $search = "";
+            }
+            if(!isset($page)) {
+                $page = 1;
+            }
+
+
+            $result =  $this->serviceWeaponEncodingRepository->findByAgentPagination($idAgent, $item_per_page, $page, $search);
+
+            $content = json_encode($result);
+            $response = new Response($content, Response::HTTP_OK, ["Content-Type" => "application/json"]);
+            return $response;
+        } catch (\Throwable $th) {
+            //throw $th;
+            $result = ["message" => $th->getMessage()];
+            $content = json_encode($result);
+            $response = new Response($content, Response::HTTP_INTERNAL_SERVER_ERROR, ["Content-Type" => "application/json"]);
+            return $response;
+        }
+    }
+
+
+
+
+
+    /**
      * @Route("api/weapon_encoding/pagination/{page}", name="app_get_weapon_encoding", methods="GET"  )
      */
     public function get_weapon_enconding($page, Request $request)
